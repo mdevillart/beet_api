6c4a8697d469747a7b92a4f45728e7b02f169949
mdevillart <mdevillart@outlook.com>
2020-08-11 06:03:42 -0300

    fixed deprecation warning from mongo

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'); 
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect('mongodb://localhost/beetapi', {
     useNewUrlParser: true,
